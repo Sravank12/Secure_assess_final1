ccaf34ceb00989b29e7c2170951a6b1e3f37e723
K.SRAVAN KUMAR <113578253+Sravank12@users.noreply.github.com>
2025-11-30 19:12:32 +1030

    Update run_app.bat

--- run_app.bat
+++ run_app.bat
@@ -1,19 +1,24 @@
 @echo off
 title Home Care Runner
 
-echo Initializing services...
-start "Dependencies" cmd /k "cd backend && pip install -r requirements.txt && echo Dependencies Installed. please close this window :)"
+echo Initializing backend dependencies...
+start "Dependencies" cmd /k "cd backend && pip install -r requirements.txt && echo Backend Dependencies Installed. Please close this window :)"
 
 pause
 
-echo Starting Backend ...
+echo Starting Backend...
 start cmd /k "cd backend && uvicorn main:app --reload"
 
-echo Starting Frontend ...
+echo Installing Frontend Dependencies...
+start "FrontendSetup" cmd /k "cd frontend && npm install && npm install -D vite && echo Frontend setup complete! Please close this window :)"
+
+pause
+
+echo Starting Frontend...
 start cmd /k "cd frontend && npm run dev"
 
 echo Launching website...
-start "" "http://localhost:3000" 
+start "" "http://localhost:3000"
 
 echo All services are ACTIVE!!!
 pause
